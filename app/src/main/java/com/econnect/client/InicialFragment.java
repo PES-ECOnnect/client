67e5ded8f579c8d3805c4c475cc6ccb81872911a
David <david.jimenez.gomariz@estudiantat.upc.edu>
2022-03-13 18:51:38 +0100

    .

--- app/src/main/java/com/econnect/client/InicialFragment.java
+++ app/src/main/java/com/econnect/client/InicialFragment.java
@@ -16,6 +16,7 @@ public class InicialFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicial, container, false);
     }
