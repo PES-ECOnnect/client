157a2f4dcaa046bba2044d66ebb9592834b1f445
thiagobulls05 <thiagomulero5@gmail.com>
2022-03-17 12:48:14 +0100

    <feat> [95, 96] (Pop ups d'alerta, text cliclable "Already have an account")

--- app/src/main/java/com/econnect/client/RegisterFragment.java
+++ app/src/main/java/com/econnect/client/RegisterFragment.java
@@ -1,5 +1,6 @@
 package com.econnect.client;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.econnect.client.databinding.FragmentRegisterBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class RegisterFragment extends Fragment {
 
@@ -30,6 +32,29 @@ public class RegisterFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user_name = binding.editTextTextPersonName.getText().toString();
+                String user_pass = binding.editTextTextPersonName.getText().toString();
+                String user_email = binding.editTextTextEmailAddress.getText().toString();
+                String user_adress = binding.editTextTextPostalAddress.getText().toString();
+
+                if (!user_adress.isEmpty() && !user_email.isEmpty() && !user_name.isEmpty() && !user_pass.isEmpty()) {
+                    //comunicarse con backend
+                    NavHostFragment.findNavController(RegisterFragment.this)
+                            .navigate(R.id.action_FirstFragment_to_SecondFragment);
+                }
+                else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setMessage("You have to fill all the fields").setTitle("WARNING");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+
+            }
+        });
+
+        binding.textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 NavHostFragment.findNavController(RegisterFragment.this)
