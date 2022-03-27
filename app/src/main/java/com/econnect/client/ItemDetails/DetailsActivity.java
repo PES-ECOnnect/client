bc5304a6a3cf4a72fe88ad1bd026e91a639d1c78
Pol Rivero <pol.rivero@estudiantat.upc.edu>
2022-03-27 16:12:04 +0200

    feat [#17] Add ProductDetailsFragment

---
+++ app/src/main/java/com/econnect/client/ItemDetails/DetailsActivity.java
@@ -0,0 +1,13 @@
+package com.econnect.client.ItemDetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/econnect/client/ItemDetails/ProductDetailsFragment.java
@@ -0,0 +1,25 @@
+package com.econnect.client.ItemDetails;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.econnect.Utilities.CustomFragment;
+import com.econnect.client.R;
+import com.econnect.client.databinding.FragmentProductDetailsBinding;
+
+public class ProductDetailsFragment extends CustomFragment<FragmentProductDetailsBinding> {
+
+    public ProductDetailsFragment() {
+        super(FragmentProductDetailsBinding.class);
+    }
+
+    @Override
+    protected void addListeners() {
+
+    }
+}
