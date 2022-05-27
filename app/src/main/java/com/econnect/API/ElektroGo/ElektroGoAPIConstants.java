af7d88f8bae066185e50b7d32c953eb08bbde05f
Pol Rivero <pol.rivero@estudiantat.upc.edu>
2022-05-27 09:50:51 +0200

    fix []: Extract token

--- app/src/main/java/com/econnect/API/ElektroGo/ElektroGoAPIConstants.java
+++ app/src/main/java/com/econnect/API/ElektroGo/ElektroGoAPIConstants.java
@@ -2,7 +2,6 @@ package com.econnect.API.ElektroGo;
 
 public interface ElektroGoAPIConstants {
     String ELEKTROGO_BASE_URL = "http://10.4.41.58:8080";
-    String ELEKTROGO_TOKEN = "34ee7e6c4c51e43ed6a7767bc717a7f9127d3d0025a0efbf6af124d15821c6ec";
     
     String CARPOOL_PATH = "/car-pooling/byCoord";
     
